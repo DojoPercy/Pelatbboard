3d1b672f0aa96eb4ea6f288eef514677b205fcb1
david ojo <ojodavid115@gmail.com>
2024-07-24 13:26:36 +0000

    second Commit David ojo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,7 @@ const config = {
     },
     extend: {
       colors: {
+        customGreen: '#027C66',
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
